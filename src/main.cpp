957d9b4f5b98365341192d45aa3eb2689a6119ad
Jackzzz126 <Jackzzz@126.com>
2019-08-06 19:52:33 +0800

    feat: update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <cstdio>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+bool small(int a, int b) {
+	return a > b;
+};
 int main()
 {
-	int v1 = 0, v2 = 0;
-	std::cout << "enter two num:";
-	std::cin>> v1 >> v2;
-	std::cout << "sum :" << v1 + v2 << endl;
+	vector<int> nums = {1, 3, 5, 9, 2, 4};
+	for(auto i = nums.begin(); i != nums.end(); i++) {
+		cout << *i << endl;
+	}
+	cout << endl;
+	sort(nums.begin(), nums.end(), small);
+	for(auto i = nums.begin(); i != nums.end(); i++) {
+		cout << *i << endl;
+	}
 	return 0;
 }
