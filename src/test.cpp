16b24e6f6cbdafd98e083deedc000067ab351c00
Jackzzz126 <Jackzzz@126.com>
2019-08-22 11:40:14 +0800

    up: test

--- src/test.cpp
+++ src/test.cpp
@@ -4,8 +4,14 @@
 TEST_CASE("util test", "util") {
 	SECTION("split str") {
 		REQUIRE(util::splitStr("", ":").size() == 0);
+
+		REQUIRE(util::splitStr("aaa:bbb", ":").size() == 2);
 		REQUIRE(util::splitStr("aaa:bbb", ":")[1] == "bbb");
+
+		REQUIRE(util::splitStr("aaa:bbb:", ":").size() == 3);
 		REQUIRE(util::splitStr("aaa:bbb:", ":")[2] == "");
+
+		REQUIRE(util::splitStr(":aaa:bbb:", ":").size() == 4);
 		REQUIRE(util::splitStr(":aaa:bbb:", ":")[3] == "");
 	}
 }
